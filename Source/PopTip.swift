79673ba1537076af61ee03b41a51c719ea9a3222
Kevin Leung <kevin.leung@expressvpn.com>
2017-08-07 11:51:26 +0800

    Remove default value

--- Source/PopTip.swift
+++ Source/PopTip.swift
@@ -492,7 +492,7 @@ open class PopTip: UIView {
   ///   - view: The view that will hold the poptip as a subview.
   ///   - frame: The originating frame. The poptip's arrow will point to the center of this frame.
   ///   - duration: Optional time interval that determines when the poptip will self-dismiss.
-  open func show(text: String, direction: PopTipDirection, maxWidth: CGFloat, in view: UIView, from frame: CGRect, duration: TimeInterval = 0) {
+  open func show(text: String, direction: PopTipDirection, maxWidth: CGFloat, in view: UIView, from frame: CGRect, duration: TimeInterval) {
     resetView()
     
     attributedText = nil
@@ -517,7 +517,7 @@ open class PopTip: UIView {
   ///   - view: The view that will hold the poptip as a subview.
   ///   - frame: The originating frame. The poptip's arrow will point to the center of this frame.
   ///   - duration: Optional time interval that determines when the poptip will self-dismiss.
-  open func show(attributedText: NSAttributedString, direction: PopTipDirection, maxWidth: CGFloat, in view: UIView, from frame: CGRect, duration: TimeInterval = 0) {
+  open func show(attributedText: NSAttributedString, direction: PopTipDirection, maxWidth: CGFloat, in view: UIView, from frame: CGRect, duration: TimeInterval) {
     resetView()
     
     text = nil
@@ -542,7 +542,7 @@ open class PopTip: UIView {
   ///   - view: The view that will hold the poptip as a subview.
   ///   - frame: The originating frame. The poptip's arrow will point to the center of this frame.
   ///   - duration: Optional time interval that determines when the poptip will self-dismiss.
-  open func show(customView: UIView, direction: PopTipDirection, in view: UIView, from frame: CGRect, duration: TimeInterval = 0) {
+  open func show(customView: UIView, direction: PopTipDirection, in view: UIView, from frame: CGRect, duration: TimeInterval) {
     resetView()
     
     text = nil
